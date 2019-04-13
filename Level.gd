c5e93174f7474b81d09cebb9cf72862f4049e18c
jude-lafitteIII <judewrs@gmail.com>
2019-04-13 12:06:50 +0100

    map loader

--- Level.gd
+++ Level.gd
@@ -1,15 +1,40 @@
 extends Spatial
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var level_map = File.new()
-	level_map.open("res://maps/map1.tanks", File.READ)
-	print(level_map.get_as_text())
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+	var level_map: File = File.new()
+	var _error = level_map.open("res://maps/map1.tanks", File.READ)
+	
+	var dimensions: Array = [level_map.get_8(), level_map.get_8()]
+	var grid : Array = []
+	
+	var next_byte: int = level_map.get_8()
+	
+	while not level_map.eof_reached():
+		grid.append(next_byte)
+		next_byte = level_map.get_8()
+		
+	if len(grid) != dimensions[0] * dimensions[1]:
+		print("Map file invalid")
+	
+	else:
+		print("Map file OK. Load proceeding")
+		
+		print_debug(dimensions)
+		print_debug(grid)
+		
+		var ground_gridmap: Node = get_node("GroundTiles")
+		
+		var row: int = 0
+		var col: int = 0
+		
+		for index in range(grid.size()):
+			row = index / dimensions[0]
+			col = index % dimensions[0]
+			
+			ground_gridmap.set_cell_item(row, 0, col, grid[index])
+			
+		var camera: Node = get_node("CenterCamera")
+		
+		camera.translate_object_local(Vector3(dimensions[1] * 4, 75, (dimensions[0] + 7) * 4))
+		camera.rotate_object_local(Vector3(1, 0, 0), -PI * 0.42)
