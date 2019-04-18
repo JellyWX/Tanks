49c0e639835c67f68fdbabe0f982525dedcb1794
jude-lafitteIII <judewrs@gmail.com>
2019-04-18 10:55:41 +0100

    switched rigidbody for kinematic body and got it to move around

--- Tank.gd
+++ Tank.gd
@@ -1,20 +1,36 @@
-extends Spatial
+extends KinematicBody
 
 var movement: Vector3 = Vector3(0, 0, 0)
+var rotated: bool = false
+
+const SPEED: int = 600
 
 func _process(tdelta: float):
-    if Input.is_action_pressed("LOCAL_FORWARD"):
-        movement.x = 1
-    elif Input.is_action_pressed("LOCAL_BACKWARD"):
-        movement.x = -1
+    movement.z = 0
+    
+    if not is_on_floor():
+        movement.x = 0
+        movement.y = -1
     else:
-        movement.x = 0   
+        movement.y = 0
+    
+        if Input.is_action_pressed("LOCAL_FORWARD"):
+            movement.x = 1
+        elif Input.is_action_pressed("LOCAL_BACKWARD"):
+            movement.x = -1
+        else:
+            movement.x = 0
 
-    translate_object_local(tdelta * 6 * movement)
+        if rotated:
+            movement = movement.rotated(self.rotation.normalized(), self.rotation.length())
+
+    move_and_slide(tdelta * SPEED * movement, Vector3(0, 1, 0))
 
     if Input.is_action_pressed("LOCAL_LEFT"):
+        rotated = true
         rotate_tank(tdelta, 1)
     elif Input.is_action_pressed("LOCAL_RIGHT"):
+        rotated = true
         rotate_tank(tdelta, -1)
 
 func rotate_tank(delta: float, direction: float):
