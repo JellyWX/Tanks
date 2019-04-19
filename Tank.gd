fc1fc1a74df31130ca3c3d50664db75fc2e4d3d3
jude-lafitteIII <judewrs@gmail.com>
2019-04-19 21:54:26 +0100

    second thread to process networking

--- Tank.gd
+++ Tank.gd
@@ -3,17 +3,31 @@ extends KinematicBody
 var movement: Vector3 = Vector3(0, 0, 0)
 var locally_controlled: bool = false
 var controller_id: int
+var moved: bool = false
+var ingame: bool = false
+
+var net_sync: Thread = Thread.new()
 
 const SPEED: int = 450
 
 onready var parent = get_parent()
 
 func _ready():
+    self.net_sync.start(self, "sync_position", null, 2)
+    
     self.rotation = Vector3(0, 0.0001, 0)
+    
+    self.ingame = true
+
+func sync_position(_none):
+    while true:
+        if self.ingame and self.moved:
+            self.parent.update_position(self)
+            self.moved = false
+        OS.delay_msec(60)
+
 
 func _process(tdelta: float):
-    var moved: bool = false
-    
     if not is_on_floor():
         self.movement.y = -1
     else:
@@ -37,9 +51,6 @@ func _process(tdelta: float):
         elif Input.is_action_pressed("LOCAL_RIGHT"):
             rotate_tank(tdelta, -1)
             moved = true
-            
-        if moved:
-            self.parent.update_position(self)
     
     else:
         var _collision = move_and_slide(tdelta * SPEED * movement, Vector3(0, 1, 0))
--- interfaces/LoadLevel.gd
+++ interfaces/LoadLevel.gd
@@ -5,8 +5,6 @@ var map_path: String
 func _pressed():
     if get_tree().get_network_unique_id() == 1:
         var level = get_tree().get_root().get_node("Level")
-    
-        level.remove_child(level.get_node("Menu"))
         
         level.send_map_change(self.map_path)
         
