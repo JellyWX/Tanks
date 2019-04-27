049d8e4720afcc6d6b1546569fdc3f837dc0e950
jude-lafitteIII <judewrs@gmail.com>
2019-04-27 22:08:01 +0100

    bouncing trial

--- entities/Shell.gd
+++ entities/Shell.gd
@@ -9,7 +9,11 @@ func _ready():
     self.start_time = OS.get_unix_time()
 
 func _physics_process(delta: float):
-    move_and_collide(Vector3(1, 0, 0).rotated(self.rotation.normalized(), self.rotation.length()) * delta * SPEED)
+    var collision: KinematicCollision = move_and_collide(Vector3(1, 0, 0).rotated(self.get_rotation().normalized(), self.get_rotation().length()) * delta * SPEED)
+    
+    if collision != null:
+        var vec: Vector3 = collision.normal
+        self.set_rotation(self.rotation.reflect(collision.normal))
     
     if OS.get_unix_time() - self.start_time > LIFETIME:
         self.get_parent().remove_child(self)
